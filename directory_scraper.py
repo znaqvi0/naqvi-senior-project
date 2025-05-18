3b659685f3c5a94bc9f371084e84233f28d82205
znaqvi0 <znaqvi2006@gmail.com>
2025-05-18 02:43:47 -0400

    added support for multiple directory URLs

--- directory_scraper.py
+++ directory_scraper.py
@@ -5,44 +5,53 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import requests
 
+FACE_DIR = 'mcd_faces'
 
-directory_url = 'https://www.mcdonogh.org/directory?Grade=12&DoSearch=1&StudSearch=1'
-driver = webdriver.Chrome()
-driver.get(directory_url)  # opens login page
+if __name__ == '__main__':
+    login_url = 'https://www.mcdonogh.org/login'
+    directory_urls = [f'https://www.mcdonogh.org/directory?Grade={grade}&DoSearch=1&StudSearch=1' for grade in range(9, 13)]
 
-print('press ENTER after logging in')
-input()
+    driver = webdriver.Chrome()
+    driver.get(login_url)  # opens login page
 
-driver.get(directory_url)
-soup = BeautifulSoup(driver.page_source, 'html.parser')
-driver.quit()
+    print('press ENTER after logging in')
+    input()
 
+    soups = []
+    # parse HTML for each URL
+    for directory_url in directory_urls:
+        driver.get(directory_url)
+        soups.append(BeautifulSoup(driver.page_source, 'html.parser'))
 
-os.makedirs('mcd_faces', exist_ok=True)
-# print(soup.prettify())
-divs = soup.find_all('div', class_='bg')
+    driver.quit()
 
-for div in divs:
-    img_url = div.get('style', '').split('\'')[1]  # split by single quotes surrounding relative image path
-    # print(img_url)
-    if not img_url:
-        continue
+    os.makedirs(FACE_DIR, exist_ok=True)
+    # print(soup.prettify())
+    divs = []
+    for soup in soups:
+        divs.extend(soup.find_all('div', class_='bg'))  # each image looks like div class="bg" in the HTML
 
-    full_url = urljoin('https://www.mcdonogh.org', img_url)
-    filepath = os.path.join('mcd_faces', os.path.basename(full_url))
+    for div in divs:
+        # full div: <div class="bg" style="background-image: url('/images/students/{year}/{person ID}.jpg'); background-position: center center"></div>
+        img_url = div.get('style', '').split('\'')[1]  # isolate relative image URL using single quotes
+        if not img_url:
+            continue
 
-    # skip if the face is already downloaded
-    if img_url.split('/')[-1] in os.listdir('mcd_faces'):
-        print(f'already downloaded {os.path.basename(full_url)}')
-        continue
+        full_url = urljoin('https://www.mcdonogh.org', img_url)
+        filepath = os.path.join(FACE_DIR, os.path.basename(full_url))
 
-    # download the image
-    try:
-        data = requests.get(full_url).content
-        with open(filepath, 'wb') as f:  # wb: write in binary mode to preserve image data
-            f.write(data)
-        print(f'downloaded {full_url}')
-    except Exception as e:
-        print(f'failed to download {full_url}')
+        # skip if the face is already downloaded
+        if img_url.split('/')[-1] in os.listdir(FACE_DIR):
+            print(f'already downloaded {os.path.basename(full_url)}')
+            continue
 
-    time.sleep(0.1)  # avoid attacking the servers
+        # download the image
+        try:
+            data = requests.get(full_url).content
+            with open(filepath, 'wb') as f:  # wb: write in binary mode to preserve image data
+                f.write(data)
+            print(f'downloaded {full_url}')
+        except Exception as e:
+            print(f'failed to download {full_url}')
+
+        time.sleep(0.1)  # avoid attacking the servers
