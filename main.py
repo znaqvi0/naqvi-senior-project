7ee2cc74f4be93ccefd6dc4c538a6a6b3bba83a2
znaqvi0 <znaqvi2006@gmail.com>
2025-05-23 02:38:10 -0400

    added main script; compares an input image's embedding to every embedding in the json

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
 import os
-import time
+import random
 
 import torch
 from PIL import Image
@@ -7,26 +8,28 @@ from PIL import Image
 from directory_scraper import FACE_DIR
 from facenet_pytorch import MTCNN, InceptionResnetV1
 
+with open('embeddings.json', 'r') as f:
+    embedding_dict = json.load(f)
+
+for person in embedding_dict:
+    embedding_dict[person] = torch.tensor(embedding_dict[person])
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f'running on {device}')
 
-resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)  # pretrained embedding generator
-mtcnn = MTCNN(image_size=160, margin=0, min_face_size=40).to(device)  # pretrained face detector
+resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
+mtcnn = MTCNN(image_size=160, margin=0, min_face_size=40).to(device)
 
-# test speed of pretrained models
-start_time = time.perf_counter()
+input_image_path = random.choice(os.listdir(FACE_DIR))  # test path
+input_image = Image.open(os.path.join(FACE_DIR, input_image_path))
 
-for img_path in os.listdir(FACE_DIR):
-    img = Image.open(os.path.join(FACE_DIR, img_path)).convert('RGB')
+input_image_cropped = mtcnn(input_image)
+if input_image_cropped is None:
+    print(f'no face detected in {input_image_path}')
 
-    img_cropped = mtcnn(img)
+input_embedding = resnet(input_image_cropped.unsqueeze(0))
 
-    if img_cropped is None:
-        print(f'no face detected in {img_path}')
-        continue
-
-    embedding = resnet(img_cropped.unsqueeze(0))
-
-end_time = time.perf_counter()
-print(f'total time: {end_time - start_time}')
-print(f'average time: {(end_time - start_time) / len(os.listdir(FACE_DIR))}')
+for person in embedding_dict:
+    similarity = torch.nn.functional.cosine_similarity(input_embedding, embedding_dict[person])
+    if similarity > 0.8:
+        print(f'{input_image_path} has a similarity of {similarity[0]} to {person}')
