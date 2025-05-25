74d6972f2a635ed71e96a7a7f7f31a53b8ae5c7e
znaqvi0 <znaqvi2006@gmail.com>
2025-05-25 18:25:12 -0400

    added live camera feed when taking a photo

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ import random
 from directory_scraper import FACE_DIR
 from embedding_generator import load_embedding_dict
 from recognition_utils import search_for_match
+from webcam_reader import save_with_timestamp
 
 embedding_dict = load_embedding_dict()
-cosine_threshold = 0.85
-euclidean_threshold = 0.55
+cosine_threshold = 0.8
+euclidean_threshold = 0.6
 
-input_image_path = random.choice(os.listdir(FACE_DIR))  # test path
-input_image_path = os.path.join(FACE_DIR, input_image_path)
+# input_image_path = random.choice(os.listdir(FACE_DIR))  # test path
+# input_image_path = os.path.join(FACE_DIR, input_image_path)
+_, input_image_path = save_with_timestamp()
 
 search_for_match(input_image_path, embedding_dict, cosine_threshold=cosine_threshold, euclidean_threshold=euclidean_threshold, ignore_filenames=True)
--- webcam_reader.py
+++ webcam_reader.py
@@ -9,20 +9,30 @@ PHOTO_DIR = 'captures'
 def take_photo(save_destination):
     # initialize webcam
     camera = cv2.VideoCapture(0)
+    cv2.namedWindow('webcam')
 
     if not camera.isOpened():
         print('failed to open camera')
         return None
 
     # read frame from webcam
-    ret, frame = camera.read()
+    while True:
+        ret, frame = camera.read()
+        cv2.imshow('webcam', frame)
 
-    if ret:
-        # save frame to save_destination
-        cv2.imwrite(save_destination, frame)
-        print(f'photo saved as {save_destination}')
-    else:
-        print('photo capture failed')
+        if not ret:
+            print('photo capture failed')
+            break
+
+        k = cv2.waitKey(1)
+        if k % 256 == 27:  # esc pressed
+            print("esc pressed, closing...")
+            break
+        elif k % 256 == 32:  # space pressed
+            # save frame to save_destination
+            cv2.imwrite(save_destination, frame)
+            print(f'photo saved as {save_destination}')
+            break
 
     # release the camera and close any OpenCV windows
     camera.release()
