bbdf436116a26c7710bb2b9c73ca98d14093ea8a
znaqvi0 <znaqvi2006@gmail.com>
2025-05-25 17:22:38 -0400

    restructured code to be more modular

--- main.py
+++ main.py
@@ -1,38 +1,15 @@
-import json
 import os
 import random
 
-import torch
-from PIL import Image
-
 from directory_scraper import FACE_DIR
-from facenet_pytorch import MTCNN, InceptionResnetV1
+from embedding_generator import load_embedding_dict
+from recognition_utils import search_for_match
 
-# load embeddings from json
-with open('embeddings.json', 'r') as f:
-    embedding_dict = json.load(f)
-
-# convert each embedding into a pytorch tensor
-for person in embedding_dict:
-    embedding_dict[person] = torch.tensor(embedding_dict[person])
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f'running on {device}')
-resnet = InceptionResnetV1(pretrained='vggface2').eval().to(device)
-mtcnn = MTCNN(image_size=160, margin=0, min_face_size=40).to(device)
+embedding_dict = load_embedding_dict()
+cosine_threshold = 0.85
+euclidean_threshold = 0.55
 
 input_image_path = random.choice(os.listdir(FACE_DIR))  # test path
-input_image = Image.open(os.path.join(FACE_DIR, input_image_path)).convert('RGB')
+input_image_path = os.path.join(FACE_DIR, input_image_path)
 
-# process input image
-input_image_cropped = mtcnn(input_image)
-if input_image_cropped is None:
-    print(f'no face detected in {input_image_path}')
-input_embedding = resnet(input_image_cropped.unsqueeze(0))
-
-# search embeddings for a match
-for person in embedding_dict:
-    # compare the cosine of the angle between the two embeddings
-    similarity = torch.nn.functional.cosine_similarity(input_embedding, embedding_dict[person])
-    if similarity > 0.8:
-        print(f'{input_image_path} has a similarity of {similarity[0]} to {person}')
+search_for_match(input_image_path, embedding_dict, cosine_threshold=cosine_threshold, euclidean_threshold=euclidean_threshold, ignore_filenames=True)
